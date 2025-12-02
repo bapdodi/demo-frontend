a80a694d17615d17baf1c0df7402b17931191bba
bapdodi <arlawjdqls12@gmail.com>
2025-12-02 18:43:11 +0900

    dd

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://161.97.130.200:8080/api/posts',
+    baseURL: 'http://161.97.130.200:30610/api/posts',
 });
 
 export default api;
